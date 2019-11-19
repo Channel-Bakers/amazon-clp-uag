f3330e1287d04018062ee41da5375d2ae0f00a36
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-11-18 21:04:03 -0800

    add tabs to routes file and bump version

--- env.js
+++ env.js
@@ -3,5 +3,5 @@ module.exports = {
 	clientPrefix: 'uag',
 	merchantID: 'A3DVC6WBVNJ5RS',
 	primaryTab: 'bundle1',
-	release: '0.5'
+	release: '0.7'
 };
--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -3,9 +3,13 @@
 import common from './common';
 import bundle1 from './bundle1';
 import bundle2 from './bundle2';
+import bundle3 from './bundle3';
+import bundle4 from './bundle4';
 
 export default {
 	common,
 	bundle1,
-	bundle2
+	bundle2,
+	bundle3,
+	bundle4
 };
