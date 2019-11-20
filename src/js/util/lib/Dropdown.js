804b9162cc1a3683d6e2ea4a629ab8e426283e13
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-11-20 09:20:32 -0800

    test price scraping

--- src/js/util/lib/Dropdown.js
+++ src/js/util/lib/Dropdown.js
@@ -35,6 +35,273 @@ export default class Dropdown {
 		);
 	}
 
+	_parsePrice(html) {
+		const MOBILE = document.querySelector('.shipable-page')
+			? document
+					.querySelector('.shipable-page')
+					.classList.contains('platform-phone')
+			: false;
+
+		const PRICE = {};
+		let price;
+
+		try {
+			const ASIN = html.getElementById('ASIN');
+			const OUT_OF_STOCK = ASIN.value === this.activeOption.asin;
+
+			if (OUT_OF_STOCK) {
+				PRICE.price = null;
+				PRICE.available = false;
+				return PRICE;
+			}
+
+			let prices = [];
+
+			if (MOBILE && /Android/.test(window.navigator.userAgent)) {
+				const MOBILE_PRICE_TABLE = html.querySelector(
+					'#newPitchPriceWrapper_feature_div'
+				);
+
+				if (MOBILE_PRICE_TABLE) {
+					const SALE_PRICE_DOLLARS = MOBILE_PRICE_TABLE.querySelector(
+						'.price-large'
+					);
+
+					let priceCents = MOBILE_PRICE_TABLE.querySelector(
+						'.price-info-superscript'
+					);
+					priceCents = priceCents
+						? priceCents.innerText
+						: '00';
+
+					if (SALE_PRICE_DOLLARS) {
+						let price = `${SALE_PRICE_DOLLARS.innerText}.${priceCents}`;
+						price = parseFloat(price);
+
+						if (!isNaN(price)) {
+							prices.push(price);
+						}
+					}
+				}
+
+				// CHECK TO SEE IF UAG IS WINNING THE BUY BOX
+				// IF NOT, WE HAVE TO 
+				const MERCHANT_ID = html.querySelector('#ftSelectMerchant').value;
+
+				if (MERCHANT_ID !== env.merchantID) {
+					// UAG is not winning the Buy Box
+					// so we can either return not available
+					// or scrape the other sellers html and look
+					// for their merchant ID and offerListing ID
+					// this._scrapeOtherSellers();
+
+					// This is the return not available method
+					PRICE.price = null;
+					PRICE.available = false;
+					return PRICE;
+				}
+			} else {
+				const PRICE_TABLE = html.querySelector('#price');
+
+				if (PRICE_TABLE) {
+					PRICE_TABLE.querySelectorAll(
+						'tr:not(#regularprice_savings):not(.aok-hidden) td > span:not(#listPriceLegalMessage):not(#ourprice_shippingmessage)'
+					).forEach(function(element) {
+						if (element.innerText.includes('$')) {
+							if (
+								'primeExclusivePricingMessage' ===
+								element.getAttribute('id')
+							) {
+								let primePrice = element
+									.querySelector('a:not(span)')
+									.innerText.trim()
+									.split('$')[1]
+									.split(' ')[0]
+									.split(',')
+									.join('');
+								primePrice = parseFloat(primePrice);
+								if (!isNaN(primePrice) && prices.length) {
+									prices.push(prices[0] - primePrice);
+								}
+							} else {
+								let thisElement = element.innerText
+									.trim()
+									.split('$')[1]
+									.split(',')
+									.join('');
+								thisElement = parseFloat(thisElement);
+								if (!isNaN(thisElement)) {
+									prices.push(thisElement);
+								}
+							}
+						}
+					});
+				}
+
+				// CHECK TO SEE IF UAG IS WINNING THE BUY BOX
+				// IF NOT, WE HAVE TO 
+				const MERCHANT_ID = html.querySelector('#merchantID').value;
+
+				if (MERCHANT_ID !== env.merchantID) {
+					// UAG is not winning the Buy Box
+					// so we can either return not available
+					// or scrape the other sellers html and look
+					// for their merchant ID and offerListing ID
+					// this._scrapeOtherSellers();
+
+					// This is the return not available method
+					PRICE.price = null;
+					PRICE.available = false;
+					return PRICE;
+				}
+			}
+
+			switch (prices.length) {
+				case 0:
+					price = null;
+					break;
+				case 1:
+					price = prices[0];
+					break;
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					price = Math.min(...prices);
+					break;
+			}
+
+			if (price) PRICE.price = price;
+
+			if (isObjectEmpty(PRICE)) {
+				PRICE.price = this.activeOption.price;
+			}
+
+			PRICE.available = true;
+
+			return PRICE;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async _scrapePrice() {
+		const ASIN = this.activeOption.asin;
+		const PROXY = window.location.host.includes('amazon')
+			? ''
+			: 'https://cors-anywhere.herokuapp.com/';
+
+		const HEADERS = new Headers({
+			'User-Agent':
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+		});
+
+		try {
+			const ASIN_REQUEST = await fetch(
+				`${PROXY}https://www.amazon.com/dp/${ASIN}?smid=${env.merchantID}&th=1&psc=1`,
+				{
+					method: 'GET',
+					headers: HEADERS,
+				}
+			);
+
+			const ASIN_RESPONSE = await ASIN_REQUEST.text();
+
+			const PARSER = new DOMParser();
+			const HTML = PARSER.parseFromString(ASIN_RESPONSE, 'text/html');
+
+			const PRICE = this._parsePrice(HTML);
+
+			return PRICE &&
+				typeof PRICE === 'object' &&
+				!isObjectEmpty(PRICE)
+				? PRICE
+				: this.activeOption.price;
+		} catch {
+			return this.activeOption.price;
+		}
+	}
+
+	// async _renderPrice() {
+	// 	const PRICE_WRAPPER = this.elements.wrapper.querySelector(
+	// 		`.${env.clientPrefix}-dropdown-price`
+	// 	);
+
+	// 	PRICE_WRAPPER.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38">
+	// 			<defs>
+	// 				<linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+	// 					<stop stop-color="#7B827B" stop-opacity="0" offset="0%"/>
+	// 					<stop stop-color="#7B827B" stop-opacity=".631" offset="63.146%"/>
+	// 					<stop stop-color="#7B827B" offset="100%"/>
+	// 				</linearGradient>
+	// 			</defs>
+	// 			<g fill="none" fill-rule="evenodd">
+	// 				<g transform="translate(1 1)">
+	// 					<path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="3" transform="rotate(293.261 18 18)">
+	// 						<animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/>
+	// 					</path>
+	// 					<circle fill="#7B827B" cx="36" cy="18" r="1" transform="rotate(293.261 18 18)">
+	// 						<animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite"/>
+	// 					</circle>
+	// 				</g>
+	// 			</g>
+	// 		</svg>`;
+
+	// 	const PRICES = await this._scrapePrice();
+
+	// 	PRICE_WRAPPER.innerHTML = '';
+
+	// 	if (typeof PRICES === 'object' && !isObjectEmpty(PRICES)) {
+	// 		if (PRICES.available) {
+	// 			Object.entries(PRICES).forEach(([key, value]) => {
+	// 				if (key !== 'available') {
+	// 					const PRICE_EL = document.createElement('span');
+	// 					PRICE_EL.classList.add(key);
+	// 					PRICE_EL.innerText = numToCurrency(value);
+
+	// 					const ATTACH_METHOD =
+	// 						key === 'salePrice' ? 'appendChild' : 'prepend';
+
+	// 					PRICE_WRAPPER[ATTACH_METHOD](PRICE_EL);
+
+	// 					if (this.elements.atc.classList.contains('disabled')) {
+	// 						this.elements.atc.classList.remove('disabled');
+	// 					}
+	// 				}
+	// 			});
+	// 		} else {
+	// 			const PRICE_EL = document.createElement('span');
+	// 			PRICE_EL.classList.add('outOfStock');
+	// 			PRICE_EL.innerText = 'Out of Stock';
+
+	// 			PRICE_WRAPPER.appendChild(PRICE_EL);
+
+	// 			if (!this.elements.atc.classList.contains('disabled')) {
+	// 				this.elements.atc.classList.add('disabled');
+	// 			}
+	// 		}
+	// 	} else {
+	// 		const PRICE_EL = document.createElement('span');
+	// 		PRICE_EL.classList.add('salePrice');
+	// 		PRICE_EL.innerText = numToCurrency(PRICES);
+
+	// 		PRICE_WRAPPER.appendChild(PRICE_EL);
+
+	// 		if (this.elements.atc.classList.contains('disabled')) {
+	// 			this.elements.atc.classList.remove('disabled');
+	// 		}
+	// 	}
+
+	// 	const PRICE_CHANGE = new CustomEvent('dropdown.price.update', {
+	// 		detail: PRICES,
+	// 	});
+
+	// 	this.params.builder.elements.wrapper.dispatchEvent(PRICE_CHANGE);
+
+	// 	return this;
+	// }
+
 	// _buildATCLink() {
 	// 	let atcUrl = new URL('https://www.amazon.com/gp/item-dispatch/');
 	// 	atcUrl.searchParams.set('submit.addToCart', 'addToCart');
@@ -78,9 +345,11 @@ export default class Dropdown {
 	// 	return CTA;
 	// }
 
-	_rebuildATCLink() {
+	_updateActiveOption() {
 		this.activeOption = this._getActiveColor();
+	}
 
+	_rebuildATCLink() {
 		const OPTION_CHANGE = new CustomEvent('dropdown.color.change', {
 			detail: this.activeOption,
 		});
@@ -304,15 +573,25 @@ export default class Dropdown {
 		const SERIES = this.elements.series;
 		const COLOR = this.elements.color;
 
-		SERIES.addEventListener('change', () => {
+		SERIES.addEventListener('change', async () => {
+			this._updateActiveOption();
 			this._rebuildColorOptions();
 			this._rebuildATCLink();
 			this.elements.image.style.backgroundImage = `url('${this.activeOption.image}')`;
+
+			const NEW_PRICE = await this._scrapePrice();
+
+			console.log(NEW_PRICE);
 		});
 
-		COLOR.addEventListener('change', () => {
+		COLOR.addEventListener('change', async () => {
+			this._updateActiveOption();
 			this._rebuildATCLink();
 			this.elements.image.style.backgroundImage = `url('${this.activeOption.image}')`;
+
+			const NEW_PRICE = await this._scrapePrice();
+
+			console.log(NEW_PRICE);
 		});
 
 		return this;
